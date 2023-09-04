92a22eb47824d19b389f6ef06e8aa4e45f96aaf4
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2023-09-04 13:08:15 +0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,370 @@
+var myQuestions = [
+  {
+    question: "1. Did you __________ in New York two years ago?",
+    answers: {
+      a: 'lives',
+      b: 'living',
+      c: 'lived',
+      d: 'live'
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "2. They __________ to a new city a year ago.",
+    answers: {
+      a: 'moved',
+      b: 'have moved',
+      c: 'moving',
+      d: 'move'
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "<h4>3.  __________ finished reading the book yet?</h4>No, not yet.",
+    answers: {
+      a: 'Did you',
+      b: 'Are you',
+      c: 'Do you',
+      d: 'Have you'
+
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "4. We __________ four quizzes so far this month.",
+    answers: {
+      a: 'have',
+      b: 'to have',
+      c: 'having',
+      d: 'have had'
+
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "5. I __________ him for six years.",
+    answers: {
+      a: 'know',
+      b: 'to know',
+      c: 'knowing',
+      d: 'have known'
+
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "6. While I was eating in the kitchen, John __________ a letter in his room.",
+    answers: {
+      a: 'was writing',
+      b: 'had written',
+      c: 'did writing',
+      d: 'have written'
+
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "7. He __________ in Jeddah for two years, but now he is living in Riyadh. ",
+    answers: {
+      a: 'was living',
+      b: "has lived",
+      c: "live'",
+      d: 'did living'
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "8. I __________ in the rain for almost fifteen minutes.",
+    answers: {
+      a: 'stand',
+      b: 'has stood',
+      c: 'have stood',
+      d: 'have been standing'
+
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "9. __________ ever visited Mexico?",
+    answers: {
+      a: 'Did you',
+      b: "Have you",
+      c: "Are you",
+      d: 'You'
+
+    },
+    correctAnswer: 'b'
+  },
+  
+  {
+    question: "10. Jamal's clothes are covered with paint. He __________ the walls since 7:00 AM.",
+    answers: {
+      a: 'painted',
+      b: 'has painted',
+      c: "has been painting",
+      d: "painting"
+
+    },
+    correctAnswer: 'c'
+  },
+  
+  {
+    question: "11. The ceiling was white. Now it is blue. Tom __________ it.",
+    answers: {
+      a: 'paint',
+      b: 'to paint',
+      c: "has been painting",
+      d: "has painted"
+
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "12. The TV is working again. Someone __________ it.",
+    answers: {
+      a: "will fix",
+      b: 'has fixed',
+      c: "was fixing",
+      d: "has been fixing'"
+
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "13. Someone __________ all my cigars.  The packet is empty.",
+    answers: {
+      a: 'has smoked',
+      b: 'to smoke',
+      c: 'has been smoking',
+      d: 'smokes'
+
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "14. You __________ too many cigarettes recently.  You should smoke less.",
+    answers: {
+      a: 'have been smoking',
+      b: 'are smoking',
+      c: 'smoked',
+      d: 'was smoking'
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "15. Today is Monday, May 29th.  Leila __________ letters all day.",
+    answers: {
+      a: "has been writing",
+      b: "have written",
+      c: "wrote",
+      d: "was writing"
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "16. How many pages of that book__________ today?",
+    answers: {
+      a: 'read',
+      b: 'were reading',
+      c: 'did you read',
+      d: 'had you read'
+
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "17. By the time I arrived at the party, Tom __________ home.",
+    answers: {
+      a: 'has gone',
+      b: 'was gone',
+      c: 'gone',
+      d: 'had already gone'
+
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "18. When Ahmad offered me a piece of chocolate, I couldn’t take it because I wasn’t hungry.  I __________ lunch.",
+    answers: {
+      a: 'had just had',
+      b: 'was having',
+      c: 'have been having',
+      d: 'have just had'
+
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "19. I’m not hungry. I __________ lunch.",
+    answers: {
+      a: "had just have",
+      b: "was having",
+      c: "have been having",
+      d: 'have just had'
+
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "20. The house was dirty. We __________ cleaned it for weeks.",
+    answers: {
+      a: "have not",
+      b: "weren't",
+      c: "had not",
+      d: 'nothing'
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "21. He __________ his work tomorrow.",
+    answers: {
+      a: "will finish",
+      b: "finished",
+      c: "have finished",
+      d: "had finished"
+
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "22. He has a truck full of bricks. He __________ a house.",
+    answers: {
+      a: "builds",
+      b: "is building",
+      c: "will build",
+      d: "is going to"
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "23. Tom will come soon. When Tom __________, we will see him.",
+    answers: {
+      a: "will come",
+      b: "is coming",
+      c: "come",
+      d: "comes"
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "<p>24. Ali: Mother! I want some chocolate. </p> Mother:  O.K. I __________ some for you.",
+    answers: {
+      a: "buy",
+      b: "am buying",
+      c: "bought",
+      d: "will buy"
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "25. As soon as the taxi __________, we will be able to leave for the airport.",
+    answers: {
+      a: "arriving'",
+      b: "will arrive",
+      c: "arrive",
+      d: "arrives"
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "26. Black clouds are overhead. I think it __________ to rain.",
+    answers: {
+      a: "will",
+      b: "is going ",
+      c: "goes",
+      d: "will be"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "27. We have a lot of information about the weather. We think it __________ rain.",
+    answers: {
+      a: "will",
+      b: "going to",
+      c: "go",
+      d: "goes"
+    },
+    correctAnswer: 'a'
+  },
+
+];
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+  function showQuestions(questions, quizContainer){
+    var output = [];
+    var answers;
+    for(var i=0; i<questions.length; i++){
+      answers = [];
+      for(choices in questions[i].answers){
+
+        answers.push(
+          '<label>'
+            + '<input type="radio" name="question'+i+'" value="'+choices+'">'+ choices + '. '+ questions[i].answers[choices] + 
+            '</label>'
+            +'<br>'
+        );
+
+      }
+      output.push(
+        '<div class="question">'  + questions[i].question + '</div>' + '<div class="answers">'  + answers.join('') + '<br>' + '</div>'
+      );
+    }
+    quizContainer.innerHTML = output.join('');
+
+  }
+
+
+  function showResults(questions, quizContainer, resultsContainer){
+    var answerContainers = quizContainer.querySelectorAll('.answers');
+    var userAnswer = '';
+    var numCorrect = 0;
+  
+    for(var i=0; i<questions.length; i++){
+
+      userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+      if(userAnswer===questions[i].correctAnswer){
+
+        numCorrect++;
+        answerContainers[i].style.color = 'green';
+      }
+      else{
+        answerContainers[i].style.color = 'red';
+      }
+    }
+    
+   resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length + " or " + (numCorrect/questions.length * 100).toFixed(2) + "%"; 
+   
+  }
+
+
+  showQuestions(questions, quizContainer);
+  
+  submitButton.onclick = function(){
+    showResults(questions, quizContainer, resultsContainer);
+  }
+
+}
